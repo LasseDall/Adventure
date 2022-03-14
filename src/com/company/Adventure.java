443bf7f4ab0a2c43c2fa60961a2c04b23e35b414
morten-l-k <mort75q3@stud.kea.dk>
2022-03-14 15:32:48 +0100

    Added all 9 rooms and set directions between them

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -22,10 +22,10 @@ public class Adventure {
                     programRunning = false;
                     break;
                 case "help":
-                    System.out.println("Du får hjælp");
+                    System.out.println("Help is here");
                     break;
                 case "look":
-                    System.out.println("Du er i rummet XXX. Det ser sådan her ud...");
+                    System.out.println("Looking around");
                     break;
                 case "go":
                     switch (choice2) {
@@ -66,10 +66,48 @@ public class Adventure {
         }
     }
 
-
-
     public static void main(String[] args) {
         Adventure obj = new Adventure();
         obj.mainMenu();
+
+        //Created all 9 rooms
+        Room room1 = new Room("Room 1", "Big hall with a big staircase in front of you");
+        Room room2 = new Room("Room 2", "You are in the kitchen, which has big black and white squares");
+        Room room3 = new Room("Room 3", "You are in the office. There is a pig parrot in the corner that calls out your name");
+        Room room4 = new Room("Room 4", "You are on the first floor in a long hallway.");
+        Room room5 = new Room("Room 5", "You are in a room, where a person sits with its back towards you in front of a fire");
+        Room room6 = new Room("Room 6", "You are in a bedroom. There is a person in the bed. A staircase leads to the upper floor");
+        Room room7 = new Room("Room 7", "You are in a big closet. There is no closet in the room");
+        Room room8 = new Room("Room 8", "You are in the dining room. There is served for 8 people");
+        Room room9 = new Room("Room 9", "You are in the library. There is an old radio playing white noise");
+
+        //Setting rooms for all rooms
+        room1.setEast(room2);
+        room1.setSouth(room4);
+
+        room2.setWest(room1);
+        room2.setEast(room3);
+
+        room3.setWest(room2);
+        room3.setSouth(room6);
+
+        room4.setNorth(room1);
+        room4.setSouth(room7);
+
+        room5.setSouth(room8);
+
+        room6.setNorth(room3);
+        room6.setSouth(room9);
+
+        room7.setNorth(room4);
+        room7.setEast(room8);
+
+        room8.setWest(room7);
+        room8.setNorth(room5);
+        room8.setEast(room9);
+
+        room9.setNorth(room6);
+        room9.setWest(room8);
+
     }
 }
