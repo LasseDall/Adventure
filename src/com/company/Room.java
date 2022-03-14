a08fcdcf6ac228189e845d759b8d325737f14710
morten-l-k <mort75q3@stud.kea.dk>
2022-03-14 15:15:06 +0100

    Made attributes and constructor method

--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class Room {
+  //Making attributes
+  private String name;
+  private String description;
+  private Room north;
+  private Room east;
+  private Room south;
+  private Room west;
+
+  //Creating constructor method
+  public Room(String name, String description){
+    this.name = name;
+    this.description = description;
+    north = null;
+    east = null;
+    south = null;
+    west = null;
+  }
 
 
 }
