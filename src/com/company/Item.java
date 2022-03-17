1f05014bb38388f66b243d7cdd380afcad6e6002
morten-l-k <mort75q3@stud.kea.dk>
2022-03-17 12:48:33 +0100

    Added item class

---
+++ src/com/company/Item.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Item {
+  //Creating attributes
+  private String name;
+  private String description;
+  private int size;
+
+  public Item(String name,String description,int size){
+    this.name = name;
+    this.description = description;
+    this.size = size;
+  }
+
+}
